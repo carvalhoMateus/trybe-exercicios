2508b29679138dfb76cacdb8d93913ef5f532ab8
“carvalhoMateus” <mateus.devweb@gmail.com>
2021-11-05 18:45:31 -0300

    Exercícios 1 e 2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-1.js
@@ -0,0 +1,30 @@
+let num1 = 4;
+let num2 = 3
+let num3 = 10
+
+let soma = (num1 + num2);
+    console.log(soma);
+
+let sub = (num1 - num2);
+    console.log(sub);
+
+let  mult = (num1 * num2);
+    console.log(mult);
+
+let div  = (num1 / num2); 
+    console.log(div);
+
+let mod =((num1 + num2) % 2 ===  0);
+    console.log(mod);
+
+    // Parte 2 
+
+if (num1 > num2 > num3){
+        console.log(num1);
+} else if (num2 > num1 > num3) {
+        console.log(num2);
+} else {
+        console.log(num3);
+}
+
+
