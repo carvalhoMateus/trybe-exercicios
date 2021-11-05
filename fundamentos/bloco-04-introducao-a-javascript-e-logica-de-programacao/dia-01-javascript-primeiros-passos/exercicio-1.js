7c0e8cd160ce0d63668560ad0bce4b80f7eb1d0d
“carvalhoMateus” <mateus.devweb@gmail.com>
2021-11-05 19:37:24 -0300

    Exercícios 1, 2 e 3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-1.js
@@ -1,30 +1,38 @@
-let num1 = 4;
-let num2 = 3
-let num3 = 10
+// Parte 1
 
-let soma = (num1 + num2);
-    console.log(soma);
+// let num1 = 4;
+// let num2 = 3
+// let num3 = 10
 
-let sub = (num1 - num2);
-    console.log(sub);
+// let soma = (num1 + num2);
+//     console.log(soma);
 
-let  mult = (num1 * num2);
-    console.log(mult);
+// let sub = (num1 - num2);
+//     console.log(sub);
 
-let div  = (num1 / num2); 
-    console.log(div);
+// let  mult = (num1 * num2);
+//     console.log(mult);
 
-let mod =((num1 + num2) % 2 ===  0);
-    console.log(mod);
+// let div  = (num1 / num2); 
+//     console.log(div);
+
+// let mod =((num1 + num2) % 2 ===  0);
+//     console.log(mod);
 
     // Parte 2 
 
-if (num1 > num2 > num3){
-        console.log(num1);
-} else if (num2 > num1 > num3) {
-        console.log(num2);
+    const num4 = 7;
+    const num5 = 4;
+    const num6 = 9;
+
+if (num4 > num5 > num6){
+        console.log(num4);
+} else if (num5> num4 > num6) {
+        console.log(num5);
 } else {
-        console.log(num3);
+        console.log(num6);
 }
+
+
 
 
